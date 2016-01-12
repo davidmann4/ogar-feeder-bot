655289f916fd8ac05253dbe1e38ccc2ac1862f4c
Camille Eyriès <camille.eyries@gmail.com>
2016-01-12 14:23:06 +0100

    Update feeder.js

--- feeder.js
+++ feeder.js
@@ -304,6 +304,7 @@ for(proxy_line in lines) {
         
     }catch(e){
         console.log('error on startup: ' + e);
+        if (e == "Error: getPartyServer wants opt.party_key") {
 		console.log("connecting to ws://127.0.0.1:443");
 		
         for(var bot_id in bots_names) {
@@ -311,5 +312,7 @@ for(proxy_line in lines) {
             bots[bot_count] =  new ExampleBot(bot_count, agent, bot_count, 'ws://' + "127.0.0.1:443");                
                    
         }     
+        
+        }
     }
 }
