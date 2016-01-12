8e1acc87b3f8697b778deae300c151cedb8be2c6
Camille Eyriès <camille.eyries@gmail.com>
2016-01-12 14:36:01 +0100

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -2,5 +2,8 @@ var config = {};
 
 config.mapserver = "ws://127.0.0.1:34343";
 config.proxies = "/var/data/socks.txt";
+config.forceConnectToIp = true;
+config.forceIp = "127.0.0.1:443"
+config.forceKey = "";
 
 module.exports = config;
