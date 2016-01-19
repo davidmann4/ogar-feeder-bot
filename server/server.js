9a6d44725a993516f71ce903f92121a4dd6b0e00
davidmann4 <daaavid.mann@gmail.com>
2016-01-19 17:43:01 +0100

    basic server functionality

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-// command server
+var app = require('http').createServer()
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8081);
+
+console.log("Starting server on port 8081");
+
+io.on('connection', function (socket) {
+
+  socket.on('login', function (data) {
+  	console.log(data);
+  	socket.join(data.user);
+  });
+
+  socket.on('pos', function (data) {
+  	console.log(data);
+  	io.emit('pos', data);
+  });
+
+  socket.on('cmd', function (data) {
+    console.log(data);
+    io.emit('cmd', data);
+  });
+
+});
