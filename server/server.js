2f6d951fcaeca7cb2ca0987505c915f0adb0b3e9
Castia994 <ItzLevvie@users.noreply.github.com>
2016-01-25 17:03:12 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,27 +5,27 @@ var fs = require('fs');
 
 app.listen(config.serverPort);
 
-console.log("Starting server on port "+config.serverPort);
+console.log("Starting server on port " + config.serverPort);
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 
-  socket.on('login', function (data) {
+  socket.on('login', function(data) {
     console.log("User connected with id:" + data.uuid);
     socket.room = data.uuid;
     socket.join(data.uuid);
 
-    if(data.type == "server"){
-      io.sockets.in(socket.room).emit("force-login", "server-booted-up");      
+    if (data.type == "server") {
+      io.sockets.in(socket.room).emit("force-login", "server-booted-up");
     }
-    
+
   });
 
-  socket.on('pos', function (data) {
+  socket.on('pos', function(data) {
     //console.log(socket.room + " : " + data);
     io.sockets.in(socket.room).emit('pos', data);
   });
 
-  socket.on('cmd', function (data) {
+  socket.on('cmd', function(data) {
     console.log(data);
     io.sockets.in(socket.room).emit('cmd', data);
   });
